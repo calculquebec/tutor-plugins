bb945972b173b935a11edd473e973fe49d69916d
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-01-26 08:31:27 -0500

    update plugin_slots for ulmo

--- plugin_slots.py
+++ plugin_slots.py
@@ -117,12 +117,40 @@ PLUGIN_SLOTS.add_items(
 )
 
 from tutormfe.hooks import MFE_APPS
+from tutor import hooks
 
 @MFE_APPS.add()
 def _add_my_mfe(mfes):
-    mfe_version = "cq/teak.dev"
+    mfe_version = "cq/ulmo.dev"
     mfes["authn"]["repository"] = "https://github.com/calculquebec/frontend-app-authn.git"  # your public/private repo link
     mfes["authn"]["version"] = mfe_version
     mfes["account"]["repository"] = "https://github.com/calculquebec/frontend-app-account.git"  # your public/private repo link
     mfes["account"]["version"] = mfe_version
     return mfes
+
+@MFE_APPS.add()
+def _add_catalog_mfe(mfes):
+    mfes["catalog"] = {
+        "repository": "https://github.com/openedx/frontend-app-catalog.git",
+        "port": 1998,
+        "version": "master", # optional, will default to the Open edX current tag.
+    }
+    return mfes
+
+catalog_mfe_url = """
+CATALOG_MICROFRONTEND_URL = "http://{{ MFE_HOST }}:{{ get_mfe('catalog').port }}/catalog"
+"""
+
+env_items = [
+    (
+        "openedx-common-settings",
+        catalog_mfe_url,
+    ),
+    (
+        "openedx-lms-common-settings",
+        "ENABLE_CATALOG_MICROFRONTEND = True",
+    ),
+]
+
+for item in env_items:
+    hooks.Filters.ENV_PATCHES.add_item(item)
