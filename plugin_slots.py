925d765ae1c1861faac3a9beb255ab97ed9c9d0f
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-01-27 16:10:33 -0500

    hide custom_footer from indigo, use cq_footer instead

--- plugin_slots.py
+++ plugin_slots.py
@@ -97,11 +97,20 @@ footer = [(
     (
         "all",
         "footer_slot",
+        """
+        {
+          op: PLUGIN_OPERATIONS.Hide,
+          widgetId: 'custom_footer',
+        }"""
+    ),
+    (
+        "all",
+        "footer_slot",
         f"""
         {{
           op: PLUGIN_OPERATIONS.Insert,
           widget: {{
-            id: 'custom_footer',
+            id: 'cq_footer',
             type: DIRECT_PLUGIN,
             RenderWidget: () => {{
                 {load_file("footer/footer.jsx")}
