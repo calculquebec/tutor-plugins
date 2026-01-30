e191ad0a9613babd4b7ff85dc69ff35c7dcff4a7
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-01-30 14:16:16 -0500

    minor tweaks to instructions and style

--- theming/home_banner/home_banner.jsx
+++ theming/home_banner/home_banner.jsx
@@ -24,14 +24,22 @@ const languages = {
   "fr-ca": {
     bannerwelcome: "Bienvenue sur evolo, la plateforme de formation de Calcul Québec.",
     bannercourses: "Vous trouverez ci-dessous les cours que nous proposons.",
-    bannerconnect: "Pour débuter, cliquez sur le lien <i>Se connecter</i>, en haut à droite de cette page, puis sur <i>CILogon</i>",
-    bannercilogon: "IMPORTANT: Vous devez ensuite sélectionner <b><u>votre</u></b> établissement académique et vous connecter avec les identifiants de votre établissement."
+    bannerconnect1: "Pour débuter, cliquez sur le lien"
+    bannerocnnect2: "Se connecter, en haut à droite de cette page, puis sur",
+    bannercilogon1: "IMPORTANT: Vous devez ensuite sélectionner",
+    bannercilogon2: "établissement académique et vous connecter avec les identifiants de votre établissement."
+    login: "Se connecter",
+    your: "votre"
   },
   en: {
     bannerwelcome: "Welcome to evolo, Calcul Québec's learning platform.",
     bannercourses: "Here are the classes that you can follow.",
-    bannerconnect: "To start, click on <i>Login</i>, at the top right of this page, then on <i>CILogon</i>",
-    bannercilogon: "IMPORTANT: You must then select <b><u>your</u></b> academic institution in the list, and connect through the credentials of your institution."
+    bannerconnect1: "To start, click on",
+    bannerconnect2: ", at the top right of this page, then on",
+    bannercilogon1: "IMPORTANT: You must then select",
+    bannercilogon2: "academic institution in the list, and connect through the credentials of your institution."
+    login: "Login",
+    your: "you"
   },
 }
 return (
@@ -53,8 +61,8 @@ return (
     <div class="cq-banner">
       {languages[language].bannerwelcome} <br/>
       {languages[language].bannercourses} <br/>
-      {languages[language].bannerconnect} <br/>
-      {languages[language].bannercilogon} <br/>
+      {languages[language].bannerconnect1} <i>{languages[language].login}</i> {languages[language].bannerconnect2} <i>CILogon.</i><br/>
+      {languages[language].bannercilogon1} <b><u>{languages[language].your}</u></b> {languages[language].bannercilogon2}<br/>
     </div>
   </>
 );
