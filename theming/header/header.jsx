29db501d58e294b4fbd731c5538df3b6e2cefbbc
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-03-23 10:20:18 -0400

    ensure video subtitles are readable

--- theming/header/header.jsx
+++ theming/header/header.jsx
@@ -432,6 +432,10 @@ return (
 	  color: #5b94e1 !important;
 	  text-decoration: none !important;
 	}
+	/* ensure the video subtitles in full screen are readable */
+	.xmodule_display.xmodule_VideoBlock .video.video-fullscreen .subtitles li {
+	  color: #aaa !important;
+	}
       `}
     </style>
     <div>
