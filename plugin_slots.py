58332db19f45be7a47773a53b65a0f16d1041129
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-08-17 12:05:33 -0400

    enable frontend-base authn layer

--- plugin_slots.py
+++ plugin_slots.py
@@ -7,7 +7,9 @@ from tutor import hooks
 
 @FRONTEND_APPS.add()
 def _enable_core_apps(apps):
-#    apps["authn"]["enabled"] = True
+    app_version = "cq/verawood-app.dev"
+    apps["authn"]["enabled"] = True
+    apps["authn"]["source"] = f"https://github.com/calculquebec/frontend-app-authn.git#{app_version}"
     apps["learner-dashboard"]["enabled"] = True
     return apps
 
